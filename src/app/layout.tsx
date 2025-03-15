c0bc2084fc6f4946d0eae595ec1af727b73b2d1a
william <williamlopez0721@gmail.com>
2025-03-15 11:52:10 -0500

    add pixel and google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat, Spectral_SC } from "next/font/google";
 import ClarityScript from "@/components/analytics/ClarityScript";
+import GoogleAnalytics from "@/components/analytics/GoogleAnalytics";
+import Script from "next/script";
+import Image from "next/image";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -27,6 +30,16 @@ export const metadata: Metadata = {
     maximumScale: 1,
     userScalable: false,
   },
+  keywords: [
+    "Inversiones",
+    "Inversiones Prado Tolima",
+    "Inversion MonteAzul Group",
+    "Mejores inversiones Prado"
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  }
 };
 
 export default function RootLayout({
@@ -36,6 +49,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="scroll-smooth">
+      <head>
+        {/* Google Analytics - Implementación oficial de Next.js */}
+        <GoogleAnalytics />
+
+        {/* Facebook Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '470327726160529');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt="No Script"
+            src="https://www.facebook.com/tr?id=1389137175102137&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body
         className={`${montserrat.variable} ${spectralSC.variable} antialiased overflow-x-hidden`}
       >
--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,39 +1,39 @@
-// 'use client';
+'use client';
 
-// import Script from 'next/script';
-// import { Suspense } from 'react';
+import Script from 'next/script';
+import { Suspense } from 'react';
 
-// function AnalyticsScript() {
-//   // Usa el ID de seguimiento de las variables de entorno
-//   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || 'G-CFBJNKW729';
+function AnalyticsScript() {
+  // Usa el ID de seguimiento de las variables de entorno
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || 'G-WJSBJLNQRT';
   
-//   return (
-//     <>
-//       {/* Global Site Tag (gtag.js) - Google Analytics */}
-//       <Script
-//         strategy="afterInteractive"
-//         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-//       />
-//       <Script
-//         id="google-analytics"
-//         strategy="afterInteractive"
-//         dangerouslySetInnerHTML={{
-//           __html: `
-//             window.dataLayer = window.dataLayer || [];
-//             function gtag(){dataLayer.push(arguments);}
-//             gtag('js', new Date());
-//             gtag('config', '${GA_TRACKING_ID}');
-//           `,
-//         }}
-//       />
-//     </>
-//   );
-// }
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
+    </>
+  );
+}
 
-// export default function GoogleAnalytics() {
-//   return (
-//     <Suspense fallback={null}>
-//       <AnalyticsScript />
-//     </Suspense>
-//   );
-// } 
+export default function GoogleAnalytics() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyticsScript />
+    </Suspense>
+  );
+} 
